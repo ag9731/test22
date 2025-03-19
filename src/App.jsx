5d0d8466aaef3c8a3cf765663890f6fc6435a556
Akash <goravanakollakash@gmail.com>
2025-03-19 23:09:57 +0530

    Added Privacy Policy

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from "./components/HomePage/Home";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Privacypolicy from "./components/PrivacyPolicy/Privacypolicy";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <section>
         <Router>
+          <ScrollToTop/>
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
